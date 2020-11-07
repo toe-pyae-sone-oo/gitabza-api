35717a5a4618fe0b8d1320239b4c058dacb181aa
Toe Oo <tpso.toepyaesoneoo@gmail.com>
2020-11-07 13:22:37 +0630

    add auth in mongodb connect

--- setup.js
+++ setup.js
@@ -2,7 +2,10 @@
 
 const mongoose = require('mongoose')
 
-const url = `${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+const dbuser = process.env.DB_USER
+const dbpwd = process.env.DB_PASS
+const dbauth = (dbuser && dbpwd) ? `${dbuser}:${dbpwd}@` : ''
+const url = `mongodb://${dbauth}${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
 
 mongoose
   .connect(url, { useNewUrlParser: true })
