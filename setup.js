1c8e81733ba4a1f09835a93bd047a058f6e254c5
Toe Oo <tpso.toepyaesoneoo@gmail.com>
2020-11-07 15:12:19 +0630

    add auth source

--- setup.js
+++ setup.js
@@ -4,8 +4,20 @@ const mongoose = require('mongoose')
 
 const dbuser = process.env.DB_USER
 const dbpwd = process.env.DB_PASS
-const dbauth = (dbuser && dbpwd) ? `${dbuser}:${dbpwd}@` : ''
-const url = `mongodb://${dbauth}${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+const dbauthSource = process.env.DB_AUTH_SOURCE
+
+let url = 'mongodb://'
+
+if (dbuser && dbpwd) {
+  url += `${dbuser}:${dbpwd}@`
+}
+
+url += `${process.env.DB_HOST}:${process.env.DB_PORT}`
+url += `/${process.env.DB_NAME}`
+
+if (dbauthSource) {
+  url += `?authSource=${dbauthSource}`
+}
 
 mongoose
   .connect(url, { useNewUrlParser: true })
